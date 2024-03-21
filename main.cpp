77c321071bd5d455715bf07eb37a0c9f4caf47cf
Jan Giritsch <jangiritsch@MacBook-Air-von-Jan.local>
2024-03-21 11:31:54 +0100

    write the funktion for sting to calc

--- main.cpp
+++ main.cpp
@@ -11,44 +11,71 @@
 using namespace std;
 calc calc;
 
+
+
+void arduinoBack(){
+    
+    int len = 0;
+    double numbers[10];
+    char operatoren[10];
+    for(int i = 0;i < 10;i++){
+      
+        char zahlen[10]; //textfeld
+        
+        char input;
+        std::cin >> input;
+        
+        if(calc.is_number(input)){
+            zahlen[i] = input;
+        }else{
+            operatoren[len] = input;
+            numbers[len] = calc.char_to_int(zahlen, i);
+            len++;
+            i = -1;
+        }
+        // if the programm is finsch
+        if(input == '='){
+            cout << calc.cl(numbers, operatoren, len) << " ist die summe\n";
+            break;
+        }
+    }
+    
+}
 //simulierte main
-int main(){
-   
-        //char zahlen[10]; // to safe all numbers in a array
+int main(int argc, char* argv[]){
         /*      exampel of my idea
          * zahlen[i] operatoren[i] zahlen[i + 1]        = 3
          * zahlen[0] operatoren[0] zahlen[1]            = 3
          *        1             +         2             = 3
          */
+    string s = "12 + 5 * 2+13=";
     int len = 0;
-        for(int i = 0;i < 10;i++){
-            double numbers[10];
-            char operatoren[10];
-            
-            char zahlen[10]; //textfeld
-            
-            char input;
-            cin >> input;
-            
-            if(calc.is_number(input)){
-                zahlen[i] = input;
+    double numbers[10];
+    char operatoren[10];
+    
+    char tempNum[10];
+    int temp = 0;
+    for(int i = 0;i < s.length();i++){
+        //cout << s[i] << "\n";
+        if(s[i] != '\u0020'){
+            //cout << s[i] << "\n";
+            if(calc.is_number(s[i])){
+                //cout << s[i] << "\n";
+                tempNum[temp] = s[i];
+                temp++;
             }else{
-                operatoren[len] = input;
-                numbers[len] = calc.char_to_int(zahlen, i);
+                operatoren[len] = s[i];
+                numbers[len] = calc.char_to_int(tempNum,temp);
                 len++;
-                i = -1;
-            }
-            // if the programm is finsch
-            if(input == '='){
-                
-                
-                //double numbers[6] = {12.0 , 11.0 , 3.0 , 20.0, 5.0, 7.0};
-                //char operatoren[5] = {'+','*','+','/','+'};
-                cout << calc.cl(numbers, operatoren, len) << " ist die summe\n";
-                break;
+                temp = 0;
+                //cout << s[i] << "\n";
             }
         }
+            
+    }
+    cout << calc.cl(numbers, operatoren, len) << " ergebnis\n";
+    //arduinoBack();
+        
         return 0;
 }
-
 
